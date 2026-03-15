cb077182e9ec4427cd1dbc0d678a1c69053b5717
monikasareeen-bit <your-github-monikasareeen@gmail.com>
2026-03-15 19:43:50 +0000

    Fix database URL parsing

--- womenconnect/settings.py
+++ womenconnect/settings.py
@@ -62,10 +62,9 @@ WSGI_APPLICATION = 'womenconnect.wsgi.application'
 # Database
 
 
+DATABASE_URL = config('DATABASE_URL', default=f'sqlite:///{BASE_DIR}/db.sqlite3')
 DATABASES = {
-    'default': dj_database_url.config(
-        default=config('DATABASE_URL', default=f'sqlite:///{BASE_DIR}/db.sqlite3')
-    )
+    'default': dj_database_url.parse(DATABASE_URL, conn_max_age=60)
 }
 
 # Password validation
