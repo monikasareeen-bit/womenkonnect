62253a0444b6249827bc837e8f1acca4e0261026
monikasareeen-bit <monikasareeen@gmail.com>
2026-03-17 00:16:32 +0530

    Update build script to include admin user creation message
    Added a message indicating the creation of the admin user.

--- build.sh
+++ build.sh
@@ -6,10 +6,12 @@ pip install -r requirements.txt
 python manage.py collectstatic --noinput
 python manage.py migrate
 
-echo "from django.contrib.auth import get_user_model; \
-User = get_user_model(); \
-username='admin'; \
-password='connectsatija'; \
-email='monikasareena@gmail.com'; \
-if not User.objects.filter(username=username).exists(): \
+echo "Creating admin user..."
+
+echo "from django.contrib.auth import get_user_model;
+User=get_user_model();
+username='admin';
+email='monikasareena@gmail.com';
+password='connectsatija';
+if not User.objects.filter(username=username).exists():
     User.objects.create_superuser(username,email,password)" | python manage.py shell
