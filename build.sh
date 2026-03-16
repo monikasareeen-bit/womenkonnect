8d942f907266ab74f8704beef5648e6ed3abdcc0
Monika <monikasareeen@gmail.com>
2026-03-17 01:16:13 +0530

    Auto create admin user

--- build.sh
+++ build.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 set -o errexit
-
 pip install -r requirements.txt
-
 python manage.py collectstatic --noinput
 python manage.py migrate
 
@@ -10,13 +8,6 @@ python manage.py shell << END
 from django.contrib.auth import get_user_model
 User = get_user_model()
 
-username="admin"
-email="monikasareena@gmail.com"
-password="connectsatija"
-
-if not User.objects.filter(username=username).exists():
-    User.objects.create_superuser(username,email,password)
-    print("Admin created successfully")
-else:
-    print("Admin already exists")
+if not User.objects.filter(username="admin").exists():
+    User.objects.create_superuser("admin","admin@example.com","admin123")
 END
