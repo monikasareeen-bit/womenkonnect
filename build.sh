309f54d5f57dcf9a702e2555ddf5f5b83d313617
Monika <monikasareeen@gmail.com>
2026-03-17 01:38:01 +0530

    Remove axes and fix CSRF for render

--- build.sh
+++ build.sh
@@ -1,13 +1,14 @@
-#!/usr/bin/env bash
-set -o errexit
-pip install -r requirements.txt
-python manage.py collectstatic --noinput
-python manage.py migrate
-
 python manage.py shell << END
 from django.contrib.auth import get_user_model
+
 User = get_user_model()
 
-if not User.objects.filter(username="admin").exists():
-    User.objects.create_superuser("admin","admin@example.com","admin123")
+try:
+    if not User.objects.filter(username="admin").exists():
+        User.objects.create_superuser("admin","admin@example.com","admin123")
+        print("Admin created")
+    else:
+        print("Admin already exists")
+except Exception as e:
+    print("Admin creation skipped:", e)
 END
