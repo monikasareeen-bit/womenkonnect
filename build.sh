bcd1b838b99d5435684339c00afc53188bf1db2d
monikasareeen-bit <monikasareeen@gmail.com>
2026-03-16 14:36:12 +0530

    Update superuser creation logic in build.sh

--- build.sh
+++ build.sh
@@ -11,9 +11,9 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 if not User.objects.filter(username=os.environ['DJANGO_SUPERUSER_USERNAME']).exists():
     User.objects.create_superuser(
-        os.environ['DJANGO_SUPERUSER_USERNAME'],
-        os.environ['DJANGO_SUPERUSER_EMAIL'],
-        os.environ['DJANGO_SUPERUSER_PASSWORD']
+        os.environ['admin'],
+        os.environ['monikasareeen@gmail.com'],
+        os.environ['Satija2026!']
     )
     print('Superuser created.')
 else:
