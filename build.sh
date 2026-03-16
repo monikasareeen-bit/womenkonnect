d7b153f4586526bb8cc9fe79c34db1300a2be3b1
monikasareeen-bit <monikasareeen@gmail.com>
2026-03-16 14:01:07 +0530

    Enhance build script to conditionally create superuser
    Added a check to create a superuser only if it doesn't exist.

--- build.sh
+++ build.sh
@@ -3,4 +3,19 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --noinput
 python manage.py migrate
-python manage.py createsuperuser --noinput
+python -c "
+import django, os
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'womenconnect.settings')
+django.setup()
+from django.contrib.auth import get_user_model
+User = get_user_model()
+if not User.objects.filter(username=os.environ['DJANGO_SUPERUSER_USERNAME']).exists():
+    User.objects.create_superuser(
+        os.environ['DJANGO_SUPERUSER_USERNAME'],
+        os.environ['DJANGO_SUPERUSER_EMAIL'],
+        os.environ['DJANGO_SUPERUSER_PASSWORD']
+    )
+    print('Superuser created.')
+else:
+    print('Superuser already exists, skipping.')
+"
