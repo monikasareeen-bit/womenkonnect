5df9bc5e229c7705c8ba4eadb4b8e01ee0f3e25c
monikasareeen-bit <monikasareeen@gmail.com>
2026-03-16 15:06:25 +0530

    Refactor build.sh to remove superuser creation script

--- build.sh
+++ build.sh
@@ -3,28 +3,3 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --noinput
 python manage.py migrate
-python -c "
-import django, os
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'womenconnect.settings')
-django.setup()
-from axes.models import AccessAttempt, AccessLog
-AccessAttempt.objects.all().delete()
-AccessLog.objects.all().delete()
-print('Axes cleared.')
-"
-python -c "
-import django, os
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'womenconnect.settings')
-django.setup()
-from django.contrib.auth import get_user_model
-User = get_user_model()
-username = os.environ['DJANGO_SUPERUSER_USERNAME']
-email = os.environ['DJANGO_SUPERUSER_EMAIL']
-password = os.environ['DJANGO_SUPERUSER_PASSWORD']
-user, created = User.objects.get_or_create(username=username, defaults={'email': email, 'is_staff': True, 'is_superuser': True})
-user.set_password(password)
-user.is_staff = True
-user.is_superuser = True
-user.save()
-print('Superuser ready.')
-"
