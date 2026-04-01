ecabe86e99a663ea82f53a85156e6d4878fd0916
Monika <monikasareeen@gmail.com>
2026-04-01 11:24:52 +0530

    Auto-activate users

--- community/views.py
+++ community/views.py
@@ -35,7 +35,7 @@ def register(request):
         form = CustomUserCreationForm(request.POST)
         if form.is_valid():
             user = form.save(commit=False)
-            user.is_active = False
+            user.is_active = True
             user.save()
 
             uid = urlsafe_base64_encode(force_bytes(user.pk))
