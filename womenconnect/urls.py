dfc98177345242b010303ce21f67850c292aa650
Monika <monikasareeen@gmail.com>
2026-04-01 12:57:19 +0530

    Add robots.txt and Category to sitemap

--- womenconnect/urls.py
+++ womenconnect/urls.py
@@ -4,12 +4,33 @@ from django.contrib.auth import views as auth_views
 from django.conf import settings
 from django.conf.urls.static import static
 from django.views.generic.base import RedirectView
+from django.contrib.sitemaps.views import sitemap
+from django.contrib.sitemaps import GenericSitemap
+from community.models import Post
+
+sitemaps = {
+    'posts': GenericSitemap({
+        'queryset': Post.objects.all(),
+        'date_field': 'updated_at',
+    }, priority=0.8),
+    'categories': GenericSitemap({
+        'queryset': Category.objects.all(),
+    }, priority=0.6),
+}
+
+def robots_txt(request):
+    content = """User-agent: *
+Allow: /
+Disallow: /sugaradmin/
+Disallow: /profile/
+Sitemap: https://womenkonnect.co.in/sitemap.xml"""
+    return HttpResponse(content, content_type="text/plain")
 
 urlpatterns = [
     path('sugaradmin/', admin.site.urls),
     path('', include('community.urls')),
     path('favicon.ico', RedirectView.as_view(url='/static/community/images/womenkonnect-logo.svg', permanent=True)),
-
+    path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='sitemap'),
     path('password-reset/',
         auth_views.PasswordResetView.as_view(
             template_name='community/auth/password_reset.html'
